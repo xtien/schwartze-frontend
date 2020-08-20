c647e00580d669664d0f72a3307770bb12f3644a
christine <christine@christine.nl>
2020-08-20 16:48:38 +0200

    fixed link back to letter page (starting slash was missing)

--- src/Text.js
+++ src/Text.js
@@ -67,13 +67,13 @@ class Text extends Component {
                     }</div>
                 <div>
                     {this.state.location != null ?
-                        <h3><Link to={'get_location_details/' + location.id}> {location.location_name}</Link></h3>
+                        <h3><Link to={'/get_location_details/' + location.id}> {location.location_name}</Link></h3>
                         : null
                     }
                 </div>
                 <div>
                     {this.state.letter != null ?
-                        <h3><Link to={'get_letter_details/' + letter.number}> Brief {letter.number}</Link></h3>
+                        <h3><Link to={'/get_letter_details/' + letter.number}> Brief {letter.number}</Link></h3>
                         : null
                     }
                 </div>
