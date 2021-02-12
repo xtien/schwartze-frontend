0d29d6df396c3f0883e95974c0c18103c13f3699
Christine <christine@christine.nl>
2021-02-12 23:58:38 +0100

    created paging backward

--- src/Page.js
+++ src/Page.js
@@ -12,6 +12,8 @@ import './css/bootstrap.css'
 import AuthenticationService from './service/AuthenticationService';
 import arrow_right from "./images/arrow_right.png";
 import three_arrow_right from "./images/three_arrow_right.png";
+import three_arrow_left from "./images/three_arrow_left.png";
+import arrow_left from "./images/arrow_left.png";
 
 class Page extends Component {
 
@@ -26,7 +28,9 @@ class Page extends Component {
 
         this.post = this.post.bind(this);
         this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
         this.nextChapter = this.nextChapter.bind(this);
+        this.previousChapter = this.previousChapter.bind(this);
 
         this.post(props.match.params.chapter, props.match.params.page)
     }
@@ -39,12 +43,30 @@ class Page extends Component {
         this.post(this.state.chapter, nextpage)
     }
 
+    previous() {
+        let previous = Math.max(parseInt(this.state.page) - 1, 1);
+        this.setState({
+            page: previous
+        })
+        this.post(this.state.chapter, previous)
+    }
+
     nextChapter() {
         let nextchapter = parseInt(this.state.chapter) + 1
         this.setState({
-            chapter: nextchapter
+            chapter: nextchapter,
+            page: 1
         })
         this.post(nextchapter, this.state.page)
+    }
+
+    previousChapter() {
+        let previouschapter = Math.max(parseInt(this.state.chapter) - 1, 1)
+        this.setState({
+            chapter: previouschapter,
+            page: 1
+        })
+        this.post(previouschapter, this.state.page)
     }
 
     post(chapter, page) {
@@ -73,18 +95,33 @@ class Page extends Component {
 
     render() {
 
-         return (
+        return (
             <div>
                 <table width='100%'>
                     <tr>
                         <td>
-                            <p className='page_header'>Chapter {this.state.chapter} &nbsp; &nbsp; &nbsp; page {this.state.page} </p></td>
+                            <button type="button"
+                                    className='btn btn-link'
+                                    onClick={this.previousChapter}>
+                                <img src={three_arrow_left} alt="back"/>
+                            </button>
+                        </td>
                         <td>
                             <button type="button"
-                                     className='btn btn-link'
-                                     onClick={this.next}>
-                            <img src={arrow_right} alt="back"/>
-                        </button>
+                                    className='btn btn-link'
+                                    onClick={this.previous}>
+                                <img src={arrow_left} alt="back"/>
+                            </button>
+                        </td>
+                        <td>
+                            <p className='page_header'>Chapter {this.state.chapter} &nbsp; &nbsp; &nbsp; page {this.state.page} </p>
+                        </td>
+                        <td>
+                            <button type="button"
+                                    className='btn btn-link'
+                                    onClick={this.next}>
+                                <img src={arrow_right} alt="back"/>
+                            </button>
                         </td>
                         <td>
                             <button type="button"
